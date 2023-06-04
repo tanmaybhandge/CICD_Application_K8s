66eb40eb5acbe33408a32fbfb28716c96a22488d
tanmaybhandge <111745693+tanmaybhandge@users.noreply.github.com>
2023-06-04 12:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+# Python file (app.py)
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Mukesh!!!'
+    return '''
+<html>
+<body>
+<center>
+<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
+<br>
+<img src="https://github.com/tanmaybhandge/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
+</center>
+</body>
+</html>
+'''
